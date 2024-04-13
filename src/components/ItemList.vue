2b412340721721f4e2fe30ccaab7260dad8c5ed7
CarlsonCarlson <carlson.cheng888@gmail.com>
2024-04-13 15:03:00 -0400

    update portfolio items

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -3,13 +3,15 @@ import Item from './AnItem.vue'
 </script>
 
 <template>
-<div>
-    <Item name="visceral.ai" description="junior ai engineer and full stack dev"/>
-    <Item name="scribeAI" description="voice i/o assistant for note taking and info lookup"/>
-    <Item name="flocal" description="technical cofounder"/>
-    <Item name="daohq" description="junior data scientist and full stack dev"/>
-    <Item name="pixl technologies" description="web dev intern"/>
-    <Item name="grahamBot" description="webscrape + make qualitative decisions on stocks"/>
-    <Item name="syno" description="founder"/>
-</div>
+  <div>
+    <Item name="cambridge ai safety hub" description="ai research scientist" />
+    <Item name="berkeley ai safety initiative" description="ai research scientist" />
+    <Item name="visceral.ai" description="junior ai engineer and full stack dev" />
+    <Item name="scribeAI" description="voice i/o assistant for note taking and info lookup" />
+    <Item name="flocal" description="technical cofounder" />
+    <Item name="daohq" description="junior data scientist and full stack dev" />
+    <Item name="pixl technologies" description="web dev intern" />
+    <Item name="grahamBot" description="webscrape + make qualitative decisions on stocks" />
+    <Item name="syno" description="founder" />
+  </div>
 </template>
